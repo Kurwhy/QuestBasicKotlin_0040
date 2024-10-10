5c65e2d893ba9c0664fde3c9a7832b44d80d2943
Wahyu <firmansyahw503@gmail.com>
2024-10-10 16:01:24 +0700

    Commit 15 (belajar NullSafety, code disini mempelajari tentang cara agar bisa mengisi data dengan null)

--- app/src/main/java/com/example/myapplication/NullSafety.kt
+++ app/src/main/java/com/example/myapplication/NullSafety.kt
@@ -11,3 +11,29 @@ fun nullSafety() {
     var nullable: String? = "You can keep a null here"
 // This is OK
     nullable = null
+
+
+    // Check for null in conditions
+    if (neverNull == null) {
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+// safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+
+
+    // notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+
+    println(strLength(neverNull)) //18
+
+}
+
+fun main() {
+    nullSafety()
+}
