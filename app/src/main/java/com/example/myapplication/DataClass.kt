1434b6d646d6d5502c6d4fadf955ba31eb5c4f0e
Wahyu <firmansyahw503@gmail.com>
2024-10-10 15:45:28 +0700

    Commit 9 (belajar Data class, code disini mempelajari tentang fungsi Equals)

---
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+
+// Data class
+// Data class adalah class yang digunakan untuk menyimpan data.
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
+// Data class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
+
+data class DataClass(
+    val id: Int,
+    var email: String
+)
+
+
+fun main() {
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+// Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) //false
+
